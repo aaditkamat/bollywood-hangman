6c801d717f2be6cdbc8496460d7a37e71e9cec63
Aadit Kamat <aadit.k12@gmail.com>
2020-12-27 20:34:37 +0800

    Allow span to have bold text

--- src/styled/Span.js
+++ src/styled/Span.js
@@ -3,4 +3,5 @@ import styled from "styled-components";
 export default styled.span`
   -webkit-text-stroke-width: 0.8px;
   color: ${props => (props.score ? "#f25042" : "inherit")};
+  font-weight: ${props => (props.bold ? "bold": "normal")};
 `;
